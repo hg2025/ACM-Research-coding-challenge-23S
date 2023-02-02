6f380e0217347898e648837396f312fd75a5f6a5
hg2025 <90288992+hg2025@users.noreply.github.com>
2023-02-01 23:56:48 -0600

    Update Stars.java

--- Stars.java
+++ Stars.java
@@ -1,7 +1,7 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Stars {
     public static void main(String[] args) {
